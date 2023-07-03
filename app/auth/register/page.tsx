2a072e16bcf4280f13eb0b931843032b1fb5ab89
Receb Osman Karaaslan <103324799+recebosman@users.noreply.github.com>
2023-07-03 13:22:41 +0300

    Login and Register section theme done

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -18,17 +18,21 @@ import {
 import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
-  email: z.string().email(),
+  name: z.string().nonempty({ message: "Please enter your name" }),
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters long" }),
 });
 
-export default function Home() {
+export default function Register() {
   const [loading, setLoading] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      name: "",
       email: "",
       password: "",
     },
@@ -48,8 +52,11 @@ export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-500">
       <div className="w-full max-w-md p-6 m-auto bg-white rounded-lg shadow-md ">
-        <div className="flex justify-center mx-auto">
+        <div className="flex justify-center mx-auto flex-col text-center">
           <h2 className="text-3xl font-semibold">Car Dealership</h2>
+          <p className="mt-2 text-sm font-semibold text-gray-700">
+            Welcome to the Car Dealership. Please register to continue.
+          </p>
         </div>
 
         <Form {...form}>
@@ -57,6 +64,23 @@ export default function Home() {
             onSubmit={form.handleSubmit(onSubmit)}
             className="mt-6 space-y-4"
           >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Write your name"
+                      {...field}
+                      disabled={loading}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <FormField
               control={form.control}
               name="email"
@@ -106,43 +130,17 @@ export default function Home() {
                     Please wait
                   </>
                 ) : (
-                  "Sign in"
+                  "Sign Up"
                 )}
               </Button>
             </div>
           </form>
         </Form>
 
-        <div className="flex items-center justify-between mt-4">
-          <span className="w-1/5 border-b  lg:w-1/5"></span>
-
-          <span className="text-xs text-center text-gray-500 uppercase hover:underline">
-            or
-          </span>
-
-          <span className="w-1/5 border-b 0 lg:w-1/5"></span>
-        </div>
-
-        <div className="flex items-center mt-6 -mx-2">
-          <button
-            type="button"
-            className="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none"
-          >
-            <svg className="w-4 h-4 mx-2 fill-current" viewBox="0 0 24 24">
-              <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"></path>
-            </svg>
-
-            <span className="hidden mx-2 sm:inline">Sign in with Google</span>
-          </button>
-        </div>
-
         <p className="mt-8 text-xs font-light text-center text-gray-400">
-          Don&apos;t have an account?{" "}
-          <Link
-            href="/auth/register"
-            className="font-semibold text-gray-700  hover:underline"
-          >
-            Create One
+          Do you have an account?{" "}
+          <Link href="/" className="font-medium text-gray-700 hover:underline">
+            Sign in
           </Link>
         </p>
       </div>
